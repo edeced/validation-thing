ac99adc5bd92b7785d56339fc9929d256e48e6ca
edeced <ed_ced@icloud.com>
2020-12-10 15:21:22 -0500

    completes form validation

--- app.js
+++ app.js
@@ -2,6 +2,15 @@
 const regForm = document.getElementById('registration-form')
 const username = document.getElementById('username')
 const usernameErr = username.nextElementSibling
+
+const email = document.getElementById('email')
+const emailErr = email.nextElementSibling
+
+const password = document.getElementById('password')
+const passwordErr = password.nextElementSibling
+
+const secPassword = document.getElementById('secPassword')
+const secPasswordErr = secPassword.nextElementSibling
 // const usernameErr = document.getElementById("usernameErr")
 
 // 2. Add event / modification
@@ -10,15 +19,35 @@ regForm.addEventListener('click', function (e) {
   e.preventDefault()
 
   // 2. Check if the value is empty
-  if (username.value === '') {
+if (username.value === '') {
     // 3. If empty, give user some feedback
     usernameErr.className = 'block bg-red-500 text-white'
   } else {
     console.log('sending to server')
+  };
+
+  if (email.value === '') {
+    emailErr.className = 'block bg-red-500 text-white'
+  } else {
+    console.log('sending to server')
+  };
+
+  if (password.value === '') {
+    passwordErr.className = 'block bg-red-500 text-white'
+  } else {
+    console.log('sending to server')
+ };
+
+  if (secPassword.value === '') {
+    secPasswordErr.className = 'block bg-red-500 text-white'
+  } else {
+    console.log('sending to server')
   }
+  
 
   //@TODO: add the validation for email
 
   //@TODO: add the validation for password
 
 })
+
