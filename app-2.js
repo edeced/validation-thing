937851548ac8670eda63ac07bb5c4dc88fe6f6ce
edeced <ed_ced@icloud.com>
2021-01-11 09:31:41 -0600

    removes testindex

--- app-2.js
+++ app-2.js
@@ -1,95 +1,77 @@
 // 1. get DOM element
 const form = document.querySelector('#form')
 
-// const input = document.querySelectorAll('#form > input')
-
-// const username = document.querySelector('#username')
-// const email = document.querySelector('#email')
-// const password = document.querySelectorAll(`input[type=password]`)
-// const password = document.querySelector('#password')
-// const password2 = document.querySelector('#password2')
-// const userInput = getFormInput('#username')
-// console.log (password2)
-// console.log (password2)
-
-// adding event listener on the form
 form.addEventListener('submit', function (e) {
   e.preventDefault()
 
-
   validateEmpty(username)
-  validateEmpty(email)
-  validateEmpty(password1)
-  validateEmpty(password2)
-  validateMinLength(username)
-  validateMinLength(email)
-  validateMinLength(password1)
-  validateMinLength(password2)
+  // validateEmpty(email)
+  // validateEmpty(password1)
+  // validateEmpty(password2)
+  // validateMinLength(username)
+  // validateMinLength(password2)
   // validateIsEmail(email)
-  // validatePassMatch(password, password2)
+  validatePassMatch(password)
 
-  //@TODO: add the validation for email (Code Challenge 5a)
-  //@TODO: Code Challenge 5b: Refactor your CC 5a to use function with the "blueprints" below
   //@TODO: Code Challenge 5c: Starting from your refactored code,
   // 1. Implement and use the validatePassMatch() function to confirm both passwords match
   // 2. Implement and use the emailValidates() function to confirm it's a valid email. See: https://stackoverflow.com/a/46181
 
- //@TODO: add the validation for email (Code Challenge 5a)
   //@TODO: Code Challenge 5b: Refactor your CC 5a to use function with the "blueprints" below
   // validateIsEmail(email)
- 
- 
-  //@TODO: add the validation for password (Code Challenge 5a)
-  //@TODO: Code Challenge 5b: Refactor your CC 5a to use function with the "blueprints" below
-  // validatePassMatch(password)
-  // validatePass2Match(password2)
 
 })
 
-function validateEmpty(input) {
-  const inputName = input.name
-  // console.log(input)
-  if (input.value==='') {
-    showError(input)
+function validateEmpty(input) { 
+  if (input.value === '') {
+    showError(input, `${input.name} is empty`)
   } else {
     showSuccess(input)
   }
-
 }
 
 function showError (input, msg) {
-  // console.log(input)
-  // console.log(input.nextElementSibling)
-  input.nextElementSibling.innerHTML = `<small class="text-white">❌ Required</small>`
+  input.nextElementSibling.innerHTML = `<small class="text-white uppercase">❌ ${msg}</small>`
   input.className='error'
 }
 
 function showSuccess (input) {
-  input.nextElementSibling.innerHTML = '<small class="text-white">✅ Thank you</small>'
-  input.className='success-input'
+  input.nextElementSibling.innerHTML = `<small class="text-white ">✅ </small>`
+  input.className= 'success-input'
 }
 
 function validateIsEmail (email) {
+  validateEmpty(email)
   // const re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  if (!email.mathc(re)) {
-    showError(email
-      )
+  // console.log(re)
+  if (emailValidates(email)) {
+    showSuccess(email)
   } else {
-  showSuccess(email)
+    showError(email)
   }
 }
+
+function emailValidates (email) {
+  const re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   
-// function validatePassMatch (password, password2) {
-//   console.log(password2)
-//   console.log(password)
-//   if (password.value===password2.value){
-//     showError(password2, 'Error, not match')
-//   }
-// } 
+  return true
+}
+  
+function validatePassMatch (password1, password2) {
+  validateEmpty(password, password2)
+  if (password1.value===password2.value){
+    showError(password2, `${input.name} Does not match`)
+    return true
+    // showError(password2, 'Error, not match')
+  } else {
+
+    return false
+  }
+} 
 
 function validateMinLength (input) {
-  console.log(input.value.length)
-  if (input.value.length < 5) {
+  // console.log(input.value.length)
+  if (input.value.length < 5 || input.value.length > 14) {
   showError(input)
   } else {
     showSuccess(input)
